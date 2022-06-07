52b90d7aa738c9f8a77d4ebeccd61d3830c8a86b
ksuzh <ksuzha1@gmail.com>
2022-06-07 16:13:58 +0200

    Example005 conditions if else

---
+++ Example005_conditions/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower == "маша")
+{
+   Console.WriteLine("Hooray"); 
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
